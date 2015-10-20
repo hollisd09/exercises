d1dbba727a80aee178a138bcd65a9d5d4af1fa7c
Danielle Hollis <hollisd09@gmail.com>
2015-10-20 14:33:01 -0500

    updated lots of files

--- that-dam-chat.js
+++ that-dam-chat.js
@@ -1,6 +1,6 @@
 var messageInput = document.getElementById("message-input");
 var clearAll = document.getElementById("clear-all");
-
+var userSelect = document.getElementById("select-user");
 var darkTheme = document.getElementsByName("dark-theme");
 var largeText = document.getElementsByName("large-text");
 
@@ -12,45 +12,66 @@ var deleteMessage = document.getElementsByClassName("delete-message");
 
 var bdy = document.getElementsByTagName("body");
 
-var outputArray = []
+var messageCount = 0;
 var editMode = false;
 
 document.getElementById("clear-all").setAttribute("disabled", true);
-console.log(document.getElementById("clear-all"));
 
+
+// User object
+var users = {
+  names: ["Danielle", "Anderson", "Mathew", "James", "Luke", "Lucas", "Steve"]
+};
+
+for (var i = 0; i < users.names.length; i++) {
+	userSelect.innerHTML += "<input type='radio' name='user' value='" + users.names[i] + "'>" + users.names[i];
+}
 
 function postMessage () {
-	var post = "<span>" + messageInput.value + "</span>";
+
+	messageCount++;
+	if (messageCount > 20) {
+		results.removeChild(results.firstChild);
+		messageCount--;
+	} 
+
+	for (var i = 0; i < users.names.length; i++) {
+		if (document.getElementsByName("user")[i].checked === true) {
+			var selectedUser = document.getElementsByName("user")[i].value;
+		}
+	}
+	
+	if (selectedUser === undefined) {
+		selectedUser = "Guest";
+	}
 	var postPart1 = "<div class='posted-message'>";
+	var time = new Date();
+	var whoDisBe = " <span class='user'>" + selectedUser + ": </span>";
+	var post = "<span>" + messageInput.value + "</span>";
 	var deleteMessage = "<button class='delete-message' type='button'>Delete</button>";
 	var editMessage = "<button class='edit-message' type='button'>Edit</button>";
 	var postPart2 = "</div>";
-	var output = (postPart1 + post + editMessage + deleteMessage + postPart2);
-	if (outputArray.length < 20) {
-		outputArray.push(output);
-	} else {
-		outputArray.shift();
-		outputArray.push(output);
-	}
-	results.innerHTML = "";
-	for (var i = 0; i < outputArray.length; i++) {
-		results.innerHTML += outputArray[i];
-	}
+	var output = (postPart1 + time.toLocaleString() + whoDisBe + post + editMessage + deleteMessage + postPart2);
 
-
+	results.innerHTML += output;
 	messageInput.value = "";
+}
 
-console.log("Delete message?", deleteMessage);
+function editMe() {
+	var editThis = document.getElementById("editme");
+	editThis.innerText = messageInput.value;
+	messageInput.value = "";
+	editThis.removeAttribute("id");
+	editMode = false;
 }
 
 messageInput.addEventListener("keypress", function (e) {
 	if (e.keyCode === 13) {
 		if (editMode === false) {
-		postMessage();
-		document.getElementById("clear-all").removeAttribute("disabled");
+			postMessage();
+			document.getElementById("clear-all").removeAttribute("disabled");
 		} else {
-			
-			editMode = false;
+			editMe();
 		}
 	}
 
@@ -58,7 +79,7 @@ messageInput.addEventListener("keypress", function (e) {
 
 var clear = function () {
 	results.innerHTML = "";
-	outputArray = [];
+	messageCount = 0;
 	document.getElementById("clear-all").setAttribute("disabled", true);
 }
 
@@ -72,58 +93,31 @@ var deletePost = function(elem) {
 }
 
 var editPost = function(elem) {
-	console.log(elem);
 	messageInput.value = elem.target.previousSibling.innerText;
+	elem.target.previousSibling.setAttribute("id", "editme");	
 }
-console.log("Delete message?", deleteMessage);
-// deleteMessage.addEventListener("click", deletePost);
 
 document.querySelector("div.posted-message button.delete-message")
 
-
-
-
 document.querySelector("body").addEventListener("click", function(event) {
-  console.log(event);
 
   // Handle the click event on any li
   if (event.target.className === "delete-message") {
-    console.log("You clicked on a button element");
     deletePost(event);
-}	else if (event.target.className === "edit-message") {
-	console.log("SOMETHINGHAPPENED");
+	}	else if (event.target.className === "edit-message") {
 	editMode = true;
 	editPost(event);
-}
+	}
+	messageInput.focus();
 });
-
 
 var yark = document.querySelector('input[name=dark-theme]');
 yark.addEventListener("change", function (event) {
-	console.log(bdy);
 	document.getElementsByTagName("body")[0].classList.toggle("darkness");
 });
 
 
-
-
-
-
-
-
-var yark = document.querySelector('input[name=large-text]');
-yark.addEventListener("change", function (event) {
-	console.log(bdy);
+var yarp = document.querySelector('input[name=large-text]');
+yarp.addEventListener("change", function (event) {
 	document.getElementById("container").classList.toggle("large-text");
 });
-
-
-
-/* This is where the edit button will be */
-
-
-
-
-
-
-
