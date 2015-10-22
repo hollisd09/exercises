581562947cc56b5fc119ad60e367380bcd159fab
MAOstrander <mathew.ostrander@gmail.com>
2015-10-22 15:48:52 -0500

    we got everything working with jquery, yay

--- that-dam-chat.js
+++ that-dam-chat.js
@@ -1,21 +1,20 @@
-var messageInput = document.getElementById("message-input");
-var clearAll = document.getElementById("clear-all");
-var userSelect = document.getElementById("select-user");
-var darkTheme = document.getElementsByName("dark-theme");
-var largeText = document.getElementsByName("large-text");
+$(document).ready(function() {
 
-var results = document.getElementById("results");
+var messageInput = $("#message-input");
+var clearAll = $("#clear-all");
+var userSelect = $("#select-user");
+var darkTheme = $("#dark-theme");
+var largeText = $("#large-text");
 
-var postedMessage = document.getElementsByClassName("posted-message");
-var deleteMessage = document.getElementsByClassName("delete-message");
+var results = $("#results");
 
-
-var bdy = document.getElementsByTagName("body");
+var postedMessage = $(".posted-message");
+var deleteMessage = $(".delete-message");
 
 var messageCount = 0;
 var editMode = false;
 
-document.getElementById("clear-all").setAttribute("disabled", true);
+$("#clear-all").attr("disabled", true);
 
 
 // User object
@@ -24,20 +23,20 @@ var users = {
 };
 
 for (var i = 0; i < users.names.length; i++) {
-	userSelect.innerHTML += "<input type='radio' name='user' value='" + users.names[i] + "'>" + users.names[i];
+	userSelect.prepend("<input type='radio' name='user' value='" + users.names[i] + "'>" + users.names[i]);
 }
 
 function postMessage () {
 
 	messageCount++;
 	if (messageCount > 20) {
-		results.removeChild(results.firstChild);
+		results.find('div:first').remove()
 		messageCount--;
 	} 
 
 	for (var i = 0; i < users.names.length; i++) {
-		if (document.getElementsByName("user")[i].checked === true) {
-			var selectedUser = document.getElementsByName("user")[i].value;
+		if ($("[name='user']")[i].checked === true) {
+			var selectedUser = $("[name='user']").eq(i).val();
 		}
 	}
 	
@@ -47,29 +46,29 @@ function postMessage () {
 	var postPart1 = "<div class='posted-message'>";
 	var time = new Date();
 	var whoDisBe = " <span class='user'>" + selectedUser + ": </span>";
-	var post = "<span>" + messageInput.value + "</span>";
+	var post = "<span>" + messageInput.val() + "</span>";
 	var deleteMessage = "<button class='delete-message' type='button'>Delete</button>";
 	var editMessage = "<button class='edit-message' type='button'>Edit</button>";
 	var postPart2 = "</div>";
 	var output = (postPart1 + time.toLocaleString() + whoDisBe + post + editMessage + deleteMessage + postPart2);
 
-	results.innerHTML += output;
-	messageInput.value = "";
+	results.append(output);
+	messageInput.val("");
 }
 
 function editMe() {
-	var editThis = document.getElementById("editme");
-	editThis.innerText = messageInput.value;
-	messageInput.value = "";
-	editThis.removeAttribute("id");
+	var editThis = $("#editme");
+	editThis.text(messageInput.val());
+	messageInput.val("");
+	editThis.removeAttr("id");
 	editMode = false;
 }
 
-messageInput.addEventListener("keypress", function (e) {
-	if (e.keyCode === 13) {
+messageInput.keypress(function (event) {
+	if (event.keyCode === 13) {
 		if (editMode === false) {
 			postMessage();
-			document.getElementById("clear-all").removeAttribute("disabled");
+			$("#clear-all").removeAttr("disabled");
 		} else {
 			editMe();
 		}
@@ -80,47 +79,30 @@ messageInput.addEventListener("keypress", function (e) {
 var clear = function () {
 	results.innerHTML = "";
 	messageCount = 0;
-	document.getElementById("clear-all").setAttribute("disabled", true);
+	$("#clear-all").attr("disabled", true);
 }
 
-clearAll.addEventListener("click", clear);
+clearAll.click(clear);
 
-var deletePost = function(elem) {
-	console.log(elem);
-	elem.target.parentNode.remove(elem);
-	console.log("What's goin on");
-	console.log("this", this);
-}
-
-var editPost = function(elem) {
-	messageInput.value = elem.target.previousSibling.innerText;
-	elem.target.previousSibling.setAttribute("id", "editme");	
-}
-
-document.querySelector("div.posted-message button.delete-message")
-
-document.querySelector("body").addEventListener("click", function(event) {
-
-  // Handle the click event on any li
-  if (event.target.className === "delete-message") {
-    deletePost(event);
-	}	else if (event.target.className === "edit-message") {
+$(document).on('click', '.edit-message', function(event) {
 	editMode = true;
-	editPost(event);
-	}
+	messageInput.val($(this).prev().text());
+	$(this).prev().attr("id", "editme");
+	messageInput.select();
+});
+
+$(document).on('click', '.delete-message', function(event) {
 	messageInput.focus();
+	$(this).parent().remove();
 });
 
-var yark = document.querySelector('input[name=dark-theme]');
-yark.addEventListener("change", function (event) {
-	document.getElementsByTagName("body")[0].classList.toggle("darkness");
+darkTheme.change(function (event) {
+	$("body").toggleClass("darkness");
+});
+
+largeText.change(function (event) {
+	$("#container").toggleClass("large-text");
 });
 
 
-var yarp = document.querySelector('input[name=large-text]');
-yarp.addEventListener("change", function (event) {
-	document.getElementById("container").classList.toggle("large-text");
-});
-
-
-
+}); //End of JQuery
